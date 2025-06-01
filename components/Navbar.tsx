b7585b1fb8eb9ce9e9bb7fb35a039657d5928f99
MogesBekele <mogesbekele32@gmail.com>
2025-06-01 14:33:51 +0300

    fix the taiwind bug

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
         />
       </a>
       <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
-        <li><a href="#top">Home</a></li>
+        <li><a  href="#top">Home</a></li>
           <li><a href="#about">About</a></li>
             <li><a href="#services">Services</a></li>
             <li><a href="#work">My Work</a></li>
