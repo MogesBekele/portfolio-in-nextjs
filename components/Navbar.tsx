97173a5c74089e7d83ccd03aa1b263a398a76ef4
MogesBekele <mogesbekele32@gmail.com>
2025-06-01 14:40:19 +0300

    added the bg image on the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,9 @@ import { assets } from '@/assets/assets'
 const Navbar = () => {
   return (
     <>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]' >
+      <Image alt='' src={assets.header_bg_color} className='w-full'/>
+    </div>
     <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
       <a href="#top">
         <Image
@@ -15,7 +18,7 @@ const Navbar = () => {
           className="w-28 cursor-pointer mr-14"
         />
       </a>
-      <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
+      <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
         <li><a  href="#top">Home</a></li>
           <li><a href="#about">About</a></li>
             <li><a href="#services">Services</a></li>
