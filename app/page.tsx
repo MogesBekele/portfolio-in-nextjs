aeb04744bf57be30f2d615fc967afe8212a5e435
MogesBekele <mogesbekele32@gmail.com>
2025-06-01 13:42:56 +0300

    import navbar to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
+import Navbar from "@/components/Navbar";
 
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Welcome to my Next.js Portfolio!
-      </h1>
-      <p className="mt-4 text-lg">
-        This is a simple portfolio built with Next.js and Tailwind CSS.
-      </p>
-      <p className="mt-2 text-sm text-gray-500">
-        Explore the code and learn more about my projects.
-      </p>
+  <Navbar/>
 
     
     </>
