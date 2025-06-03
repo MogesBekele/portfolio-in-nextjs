459d972912b39c68f2c948f1d32fe832ae9586d6
MogesBekele <mogesbekele32@gmail.com>
2025-06-03 14:13:00 +0300

    mount the header component to the main page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import Image from 'next/image'
-import { assets } from '@/assets/assets'
+import React from "react";
+import Image from "next/image";
+import { assets } from "@/assets/assets";
 
 const Header = () => {
   return (
     <div>
-      
       <div>
-        <Image alt='' src={assets.profile_img} className='rounded-full w-32'/>
-        <h3 className='flex items-center gap-2 text-xl md:text-2xl mb-3 font-font-ovo '>Hi I'm Moges Bekele <Image src={assets.hand_icon} alt='' className='w-6'/> </h3>
+        <Image alt="" src={assets.profile_img} className="rounded-full w-32" />
+      </div>
+      <h3 className="flex items-center gap-2 text-xl md:text-2xl mb-3 font-font-ovo ">
+        Hi I'm Moges Bekele
+        <Image src={assets.hand_icon} alt="" className="w-6" />
+      </h3>
+      <h1 className="text-3xl sm:text-6xl lg:text-[66px] ">Fullstack developer based on Ethoipia</h1>
+      <p className="max-w-2xl mx-auto ">I am Fullstack developer from Ethoipia </p>
+      <div className="flex flex-col sm:flex-row items-center gap-4 " >
+        <a href="#contact" className="px-10 py-3 border rounded-full border-white bg-black text-white flex items-center gap-2">Contact me   <Image src={assets.right_arrow_white} alt="" className="w-4" /></a>
+          <a href="/sample-resume.pdf" download className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2">My resume   <Image src={assets.download_icon} alt="" className="w-4" /></a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
