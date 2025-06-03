b6f22bc46e732614a28a9dc8a2338340241a2b22
MogesBekele <mogesbekele32@gmail.com>
2025-06-03 14:46:31 +0300

    improve the styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,11 @@ import { assets } from "@/assets/assets";
 
 const Header = () => {
   return (
-    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
+    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4 font-display">
       <div>
         <Image alt="" src={assets.profile_img} className="rounded-full w-32" />
       </div>
-      <h3 className="flex items-center gap-2 text-xl md:text-2xl mb-3 font-font-ovo ">
+      <h3 className="flex items-center gap-2 text-xl md:text-2xl mb-3 font-display">
         Hi I'm Moges Bekele
         <Image src={assets.hand_icon} alt="" className="w-6" />
       </h3>
