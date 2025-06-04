8a1622d981a652e806d9437f9681fa44898b00c0
MogesBekele <mogesbekele32@gmail.com>
2025-06-04 15:32:13 +0300

    create the about component

--- components/About.tsx
+++ components/About.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import { assets } from "@/assets/assets";
 
 const About = () => {
   return (
-    <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
-      <h4 className='text-center mb-2 text-lg font-main'>Inroduction</h4>
-      <h2 className='text-center text-5xl font-main'>About me</h2>
-      
+    <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-main">Inroduction</h4>
+      <h2 className="text-center text-5xl font-main">About me</h2>
+      <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
+        <div>
+          <Image
+            src={assets.user_image}
+            alt=""
+            className="w-full rounded-3xl"
+          />
+        </div>
+        <div className="flex-1 ">
+       
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
