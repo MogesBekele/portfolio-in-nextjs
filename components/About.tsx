41794088ab69f5fa7502ba9d724f2e7ae6edaef1
MogesBekele <mogesbekele32@gmail.com>
2025-06-04 15:44:36 +0300

    apply the tailwind css on the map section

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { assets } from "@/assets/assets";
+import { assets, infoList } from "@/assets/assets";
 
 const About = () => {
   return (
@@ -8,7 +8,7 @@ const About = () => {
       <h4 className="text-center mb-2 text-lg font-main">Inroduction</h4>
       <h2 className="text-center text-5xl font-main">About me</h2>
       <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
-        <div>
+        <div className="w-64 sm:w-80 rounded-3xl max-w-none">
           <Image
             src={assets.user_image}
             alt=""
@@ -16,6 +16,19 @@ const About = () => {
           />
         </div>
         <div className="flex-1 ">
+          <p className=" mb-10 max-w-2xl font-main">
+            i am exprienced Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium sunt, sit officia quisquam ex minus. Neque quis cum quo vitae laboriosam consequuntur. Iste accusamus atque sequi delectus deleniti magni culpa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, facere.
+          </p>
+          <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
+            {infoList.map(({icon, iconDark, title, description}, index)=>(
+              <li className="border-[.5px] border-gray-400 rounded-xl p-6 cursor-pointer" key={index}>
+                <Image src={icon} alt={title}/>
+                <h3>{title}</h3>
+                <p>{description}</p>
+              </li>
+            ))}
+          </ul>
+
        
         </div>
       </div>
