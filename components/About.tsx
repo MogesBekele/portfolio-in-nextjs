998dca7fb802164c7f6018db3bf03175613f2ce6
MogesBekele <mogesbekele32@gmail.com>
2025-06-04 16:32:43 +0300

    apply the bg position

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { assets, infoList } from "@/assets/assets";
+import { assets, infoList, toolsData } from "@/assets/assets";
 
 const About = () => {
   return (
@@ -25,6 +25,16 @@ const About = () => {
                 <Image className="w-7 mt-3" src={icon} alt={title}/>
                 <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
                 <p className="text-gray-600 text-sm">{description}</p>
+              </li>
+            ))}
+          </ul>
+          <h4 className="my-6 text-gray-700 font-main">Tools I use</h4>
+          <ul className="flex items-center gap-3 sm:gap-5">
+            {toolsData.map((tool, index)=>(
+              <li className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg
+              cursor-pointer hover:-translate-y-1 duration-500" key={index}>
+
+                <Image src={tool} alt="tool" className="w-5 sm:w-7"/>
               </li>
             ))}
           </ul>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { assets } from "@/assets/assets";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isSroll, setIsScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (scrollY > 50) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    });
+  }, []);
 
   return (
     <>
       <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
         <Image alt="" src={assets.header_bg_color} className="w-full" />
       </div>
-      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 bg-white/80 backdrop-blur-md">
+      <nav className={ `w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50  ${isSroll? 'bg-white bg-opacity-50 backdrop-blur-lg shadow-sm':""}`}>
         <a href="#top">
           <Image
             src={assets.logo}
-            alt="Logo"
-            width={112}
-            height={40}
+            alt=""
             className="w-28 cursor-pointer mr-14"
           />
         </a>
-        <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
+        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isSroll?"":" bg-white shadow-sm bg-opacity-50 "}`}>
           <li>
             <a href="#top">Home</a>
           </li>
@@ -70,19 +79,29 @@ const Navbar = () => {
             />
           </div>
           <li>
-            <a onClick={() => setMenuOpen(false)} href="#top">Home</a>
+            <a onClick={() => setMenuOpen(false)} href="#top">
+              Home
+            </a>
           </li>
           <li>
-            <a onClick={() => setMenuOpen(false)} href="#about">About</a>
+            <a onClick={() => setMenuOpen(false)} href="#about">
+              About
+            </a>
           </li>
           <li>
-            <a onClick={() => setMenuOpen(false)} href="#services">Services</a>
+            <a onClick={() => setMenuOpen(false)} href="#services">
+              Services
+            </a>
           </li>
           <li>
-            <a onClick={() => setMenuOpen(false)} href="#work">My Work</a>
+            <a onClick={() => setMenuOpen(false)} href="#work">
+              My Work
+            </a>
           </li>
           <li>
-            <a onClick={() => setMenuOpen(false)} href="#contact">Contact</a>
+            <a onClick={() => setMenuOpen(false)} href="#contact">
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
