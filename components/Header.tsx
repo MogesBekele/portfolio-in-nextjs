04433f6a709a7a0cf40531207f432a31b1197626
MogesBekele <mogesbekele32@gmail.com>
2025-06-04 16:59:32 +0300

    apply the tailwind css

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,15 +4,15 @@ import { assets } from "@/assets/assets";
 
 const Header = () => {
   return (
-    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4 font-ovo pt-32">
+    <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4 pt-32">
       <div>
         <Image alt="" src={assets.profile_img} className="rounded-full w-32" />
       </div>
-      <h3 className="flex items-center gap-2 text-xl md:text-2xl mb-3 font-display">
+      <h3 className="flex items-center gap-2 text-xl md:text-2xl mb-3 ">
         Hi I'm Moges Bekele
         <Image src={assets.hand_icon} alt="" className="w-6" />
       </h3>
-      <h1 className="text-3xl sm:text-6xl lg:text-[66px] ">
+      <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-main">
         Fullstack developer based on Ethoipia
       </h1>
       <p className="max-w-2xl mx-auto ">
