c9c76007df8363c57ea1b5514e4961489f2bf2dd
MogesBekele <mogesbekele32@gmail.com>
2025-06-04 16:00:45 +0300

    add the shadow on the descption

--- components/About.tsx
+++ components/About.tsx
@@ -21,7 +21,7 @@ const About = () => {
           </p>
           <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
             {infoList.map(({icon, iconDark, title, description}, index)=>(
-              <li className="border-[.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500" key={index}>
+              <li className="border-[.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black" key={index}>
                 <Image className="w-7 mt-3" src={icon} alt={title}/>
                 <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
                 <p className="text-gray-600 text-sm">{description}</p>
