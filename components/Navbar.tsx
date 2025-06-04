59aeb50f2f9a38c4fa428b1ed0a37b8ee76b5ed4
MogesBekele <mogesbekele32@gmail.com>
2025-06-04 16:42:26 +0300

    fix the navbar section

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,7 +21,11 @@ const Navbar = () => {
       <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
         <Image alt="" src={assets.header_bg_color} className="w-full" />
       </div>
-      <nav className={ `w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50  ${isSroll? 'bg-white bg-opacity-50 backdrop-blur-lg shadow-sm':""}`}>
+      <nav
+        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50  ${
+          isSroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""
+        }`}
+      >
         <a href="#top">
           <Image
             src={assets.logo}
@@ -29,7 +33,11 @@ const Navbar = () => {
             className="w-28 cursor-pointer mr-14"
           />
         </a>
-        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isSroll?"":" bg-white shadow-sm bg-opacity-50 "}`}>
+        <ul
+          className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${
+            isSroll ? "" : " bg-white shadow-sm bg-opacity-50 "
+          }`}
+        >
           <li>
             <a href="#top">Home</a>
           </li>
