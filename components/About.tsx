8c15e12bd0e99ebd5e91c16a6fb64d324c2720c5
MogesBekele <mogesbekele32@gmail.com>
2025-06-05 14:50:32 +0300

    add the detials

--- components/About.tsx
+++ components/About.tsx
@@ -5,8 +5,8 @@ import { assets, infoList, toolsData } from "@/assets/assets";
 const About = () => {
   return (
     <div id="about" className="w-full px-[12%] py-10 scroll-mt-20 pt-32">
-      <h4 className="text-center mb-2 text-lg font-main">Inroduction</h4>
-      <h2 className="text-center text-5xl font-main">About me</h2>
+      <h4 className="text-center mb-2 text-lg font-ovo">Inroduction</h4>
+      <h2 className="text-center text-5xl font-ovo">About me</h2>
       <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
         <div className="w-64 sm:w-80 rounded-3xl max-w-none">
           <Image
--- components/Servives.tsx
+++ components/Servives.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Servives = () => {
   return (
     <div id='services' className="w-full px-[12%] py-10 scroll-mt-20 pt-32">
-       <h4 className="text-center mb-2 text-lg font-main">What I offer </h4>
-      <h2 className="text-center text-5xl font-main">My Services</h2>
+       <h4 className="text-center mb-2 text-lg font-ovo">What I offer </h4>
+      <h2 className="text-center text-5xl font-ovo">My Services</h2>
+      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius non blanditiis libero accusamus molestiae, maxime suscipit eos provident facilis voluptas ex distinctio saepe, </p>
       
     </div>
   )
