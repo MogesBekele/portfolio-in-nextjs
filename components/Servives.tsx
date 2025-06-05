71a26bc4ce69023323487b78c90a4a1fcd4e2082
MogesBekele <mogesbekele32@gmail.com>
2025-06-05 15:00:30 +0300

    apply the tailwind css

--- components/Servives.tsx
+++ components/Servives.tsx
@@ -1,5 +1,6 @@
+import { assets, serviceData } from '@/assets/assets'
 import React from 'react'
-
+import Image from "next/image";
 const Servives = () => {
   return (
     <div id='services' className="w-full px-[12%] py-10 scroll-mt-20 pt-32">
@@ -7,6 +8,22 @@ const Servives = () => {
       <h2 className="text-center text-5xl font-ovo">My Services</h2>
       <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius non blanditiis libero accusamus molestiae, maxime suscipit eos provident facilis voluptas ex distinctio saepe, </p>
       
+      <div className='grid grid-cols-4 gap-6 my-10'>
+
+        {
+          serviceData.map(({icon, title, description, link}, index)=>(
+            <div key={index}> 
+
+              <Image src={icon} alt='' className='w-10'/>
+              <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
+              <p className='text-sm text-gray-600 leading-5'>{description}</p>
+              <a href={link}>Read more <Image alt='' src={assets.right_arrow} className='w-4'/></a>
+            </div>
+
+          ))
+        }
+
+      </div>
     </div>
   )
 }
