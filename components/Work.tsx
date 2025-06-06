76831453b96e4769d768b467fdc01182e5412456
MogesBekele <mogesbekele32@gmail.com>
2025-06-06 15:13:15 +0300

    add the descriptoion on the work component

--- components/Work.tsx
+++ components/Work.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Work = () => {
   return (
     <div id='work' className="w-full px-4 md:px-[8%] py-10 scroll-mt-20 pt-32">
-       <h4 className="text-center mb-2 text-lg font-ovo">What I offer </h4>
-      <h2 className="text-center text-5xl font-ovo">My Services</h2>
+       <h4 className="text-center mb-2 text-lg font-ovo">My portfolio</h4>
+      <h2 className="text-center text-5xl font-ovo">My latest work</h2>
       <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo'>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius non blanditiis libero accusamus molestiae, maxime suscipit eos provident facilis voluptas ex distinctio saepe,
+      welcome to my web development portfolio Explore a collection of projects showcasing my expertise in fullstack development
       </p>
     </div>
   )
