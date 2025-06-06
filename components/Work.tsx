c41add2dc25215f2d46583dbaed875de2eac64b2
MogesBekele <mogesbekele32@gmail.com>
2025-06-06 15:54:18 +0300

    added the project card

--- components/Work.tsx
+++ components/Work.tsx
@@ -11,22 +11,26 @@ const Work = () => {
         welcome to my web development portfolio Explore a collection of projects
         showcasing my expertise in fullstack development
       </p>
-      <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-2 gap-5 my-10'>
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-2 gap-6 my-10">
         {workData.map((project, index) => (
-          <div className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
+          <div
+            className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
             key={index}
             style={{ backgroundImage: `url(${project.bgImage})` }}
           >
-            <div>
-              <h2>{project.title}</h2>
-              <p>{project.description}</p>
-            </div>
-            <div>
-              <Image alt="send icon" src={assets.send_icon} className="w-5"/>
+            <div className="bg-white w-9/12 rounded-md absolute bottom-5 left-1/7  -traslate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+              <div>
+                <h2 className="font-semibold">{project.title}</h2>
+                <p className="text-sm text-gray-700">{project.description}</p>
+              </div>
+              <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transtion">
+                <Image alt="send icon" src={assets.send_icon} className="w-5" />
+              </div>
             </div>
           </div>
         ))}
       </div>
+      <a href="" className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] rounded-full y-3 px-10 mx-auto my-20 hover:bg-lime-300 duration-500">show more <Image src={assets.right_arrow_bold} alt="right arrow " className="w-4"/> </a>
     </div>
   );
 };
