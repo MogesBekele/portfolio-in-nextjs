a0f0b39fe431f0b7063e8c0e2b33aeb6747b51d5
MogesBekele <mogesbekele32@gmail.com>
2025-06-07 16:16:16 +0300

    add the functionaly for the web3form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { assets } from "@/assets/assets";
 
 const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "161d4ae9-caa4-496c-bda2-e23aeee07b69");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  }; 
+
   return (
     <div
       id="contact"
@@ -14,16 +39,21 @@ const Contact = () => {
         I'd love to hear from you! If you have any questions, comments, or
         feedback please use the form below
       </p>
-      <form className="max-w-2xl mx-auto flex flex-col gap-6 bg-white/80 p-8 rounded-lg shadow">
+      <form
+        onSubmit={onSubmit}
+        className="max-w-2xl mx-auto flex flex-col gap-6 bg-white/80 p-8 rounded-lg shadow"
+      >
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <input
             type="text"
+            name="name"
             placeholder="Enter your name"
             required
             className="border border-gray-300 rounded-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
           />
           <input
             type="email"
+            name="email"
             placeholder="Enter your email"
             required
             className="border border-gray-300 rounded-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
@@ -32,16 +62,19 @@ const Contact = () => {
         <textarea
           rows={6}
           placeholder="Enter your message"
+          name="message"
           required
           className="border border-gray-300 rounded-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 transition resize-none"
         ></textarea>
         <button
           type="submit"
-        className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80
+          className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80
          text-white rounded-full mx-auto hover:bg-black duration-500"
         >
-          Submit now <Image alt="" src={assets.right_arrow_white} className="w-4"/>
+          Submit now{" "}
+          <Image alt="" src={assets.right_arrow_white} className="w-4" />
         </button>
+        <p className="mt-4">{result}</p>
       </form>
     </div>
   );
