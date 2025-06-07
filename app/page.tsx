94378fc0345fb47e2c9d809412d2a9c26bdf0ba0
MogesBekele <mogesbekele32@gmail.com>
2025-06-07 16:29:35 +0300

    create the footer detials

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import About from "@/components/About";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Servives from "@/components/Servives";
@@ -15,6 +16,7 @@ export default function Home() {
       <Servives/>
       <Work/>
       <Contact/>
+      <Footer/>
     </>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+
 const Footer = () => {
   return (
-    <div>
+    <div className='mt-20'>
+      <div className='text-center'>  
+        <h1 className="w-28 font-semibold text-3xl cursor-pointer mr-14 font-ovo">
+            Moges<span className="text-red-700">.</span>
+          </h1>
+      </div>
       
     </div>
   )
