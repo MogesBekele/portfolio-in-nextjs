87c186fcc8a16634c2a53feaef59950ee833029b
MogesBekele <mogesbekele32@gmail.com>
2025-06-07 15:41:56 +0300

    inport bg image in the contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div  id='contact' className="w-full px-4 md:px-[8%] py-10 scroll-mt-20 pt-32">
-        <h4 className="text-center mb-2 text-lg font-ovo">What I offer </h4>
-      <h2 className="text-center text-5xl font-ovo">My Services</h2>
-      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo'>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius non blanditiis libero accusamus molestiae, maxime suscipit eos provident facilis voluptas ex distinctio saepe,
+    <div
+      id="contact"
+      className="w-full px-4 md:px-[8%] py-10 scroll-mt-20 pt-32 bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-[length:90%_auto]"
+    >
+      <h4 className="text-center mb-2 text-lg font-ovo">Connect with me</h4>
+      <h2 className="text-center text-5xl font-ovo">Get in touch</h2>
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius non
+        blanditiis libero accusamus molestiae, maxime suscipit eos provident
+        facilis voluptas ex distinctio saepe,
       </p>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
