b2a48c816320176ab995ec948f42047945427d3d
MogesBekele <mogesbekele32@gmail.com>
2025-06-07 15:32:21 +0300

    mount the contact page on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import About from "@/components/About";
+import Contact from "@/components/Contact";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Servives from "@/components/Servives";
@@ -13,6 +14,7 @@ export default function Home() {
       <About/>
       <Servives/>
       <Work/>
+      <Contact/>
     </>
   );
 }
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      
+    <div  id='contact' className="w-full px-4 md:px-[8%] py-10 scroll-mt-20 pt-32">
+        <h4 className="text-center mb-2 text-lg font-ovo">What I offer </h4>
+      <h2 className="text-center text-5xl font-ovo">My Services</h2>
+      <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius non blanditiis libero accusamus molestiae, maxime suscipit eos provident facilis voluptas ex distinctio saepe,
+      </p>
     </div>
   )
 }
