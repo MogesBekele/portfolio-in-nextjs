6817121e14c9a288e44d431d6ceb03eae304f739
MogesBekele <mogesbekele32@gmail.com>
2025-06-07 16:42:18 +0300

    added the footer descr

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
+import { assets } from "@/assets/assets";
 
 const Footer = () => {
   return (
-    <div className='mt-20'>
-      <div className='text-center'>  
-        <h1 className="w-28 font-semibold text-3xl cursor-pointer mr-14 font-ovo">
-            Moges<span className="text-red-700">.</span>
-          </h1>
+    <div className="mt-20">
+      <div className="text-center">
+        <h1 className="w-36 mx-auto mb-2 font-semibold text-3xl cursor-pointer font-ovo">
+          Moges<span className="text-red-700">.</span>
+        </h1>
+        <div className="w-max flex items-center gap-2 mx-auto">
+          <Image alt="" src={assets.mail_icon} className="w-6"/>
+          mogesbekele32@gmail.com
+        </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
