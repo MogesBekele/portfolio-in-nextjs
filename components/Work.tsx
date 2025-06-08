dadd195c6ddeeae9a2120ea36f6bc01179b91256
MogesBekele <mogesbekele32@gmail.com>
2025-06-08 18:34:42 +0300

    complete the animation on the work section

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,18 +1,41 @@
 import { assets, workData } from "@/assets/assets";
 import React from "react";
 import Image from "next/image";
+import { motion } from "motion/react";
 
 const Work = () => {
   return (
-    <div id="work" className="w-full px-4 md:px-[8%] py-10 scroll-mt-20 pt-32">
-      <h2 className="text-center text-5xl font-ovo">My latest work</h2>
-      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    
+    id="work" className="w-full px-4 md:px-[8%] py-10 scroll-mt-20 pt-32">
+      <motion.h2
+          initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+      
+      className="text-center text-5xl font-ovo">My latest work</motion.h2>
+      <motion.p
+        initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 0.7, delay: 0.5}}
+
+    
+      className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
         welcome to my web development portfolio Explore a collection of projects
         showcasing my expertise in fullstack development
-      </p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-2 gap-6 my-10 m-6">
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 0.9, delay: 0.6 }}
+      className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-2 gap-6 my-10 m-6">
         {workData.map((project, index) => (
-          <div
+          <motion.div
+          whileHover={{scale: 1.05}}
+          transition={{duration: 0.3}}
             className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
             key={index}
             style={{ backgroundImage: `url(${project.bgImage})` }}
@@ -26,10 +49,13 @@ const Work = () => {
                 <Image alt="send icon" src={assets.send_icon} className="w-5" />
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
-      </div>
-      <a
+      </motion.div>
+      <motion.a
+        initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.1, delay: 0.5 }}
         href=""
         className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.2px] rounded-full py-3 px-10 mx-auto my-20 hover:bg-amber-50 duration-500"
       >
@@ -39,8 +65,8 @@ const Work = () => {
           alt="right arrow "
           className="w-4"
         />{" "}
-      </a>
-    </div>
+      </motion.a>
+    </motion.div>
   );
 };
 
