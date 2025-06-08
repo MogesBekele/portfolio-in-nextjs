e0806214a38748e79c10305af4ca2da3cd9cbd4b
MogesBekele <mogesbekele32@gmail.com>
2025-06-08 18:19:19 +0300

    complete the about section animation

--- components/About.tsx
+++ components/About.tsx
@@ -1,27 +1,57 @@
 import React from "react";
 import Image from "next/image";
 import { assets, infoList, toolsData } from "@/assets/assets";
-import {motion} from 'motion/react'
+import { motion } from "motion/react";
 
 const About = () => {
   return (
     <motion.div
-          initial={{  opacity: 0 }}
-          whileInView={{  opacity: 1 }}
-          transition={{ duration: 1 }}
-    
-    id="about" className="w-full px-[12%] py-10 scroll-mt-20 pt-32">
-      <h4 className="text-center mb-2 text-lg font-ovo">Inroduction</h4>
-      <h2 className="text-center text-5xl font-ovo">About me</h2>
-      <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
-        <div className="w-64 sm:w-80 rounded-3xl max-w-none">
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id="about"
+      className="w-full px-[12%] py-10 scroll-mt-20 pt-32"
+    >
+      <motion.h4
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.3 }}
+        className="text-center mb-2 text-lg font-ovo"
+      >
+        Inroduction
+      </motion.h4>
+      <motion.h2
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className="text-center text-5xl font-ovo"
+      >
+        About me
+      </motion.h2>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+        className="flex w-full flex-col lg:flex-row items-center gap-20 my-20"
+      >
+        <motion.div
+          initial={{ scale: 0.9, opacity: 0 }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          className="w-64 sm:w-80 rounded-3xl max-w-none"
+        >
           <Image
             src={assets.user_image}
             alt=""
             className="w-full rounded-3xl"
           />
-        </div>
-        <div className="flex-1 ">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.8 }}
+          className="flex-1 "
+        >
           <p className=" mb-10 max-w-2xl font-ovo">
             i am exprienced Lorem ipsum dolor sit amet consectetur adipisicing
             elit. Laudantium sunt, sit officia quisquam ex minus. Neque quis cum
@@ -29,18 +59,24 @@ const About = () => {
             delectus deleniti magni culpa! Lorem ipsum dolor sit amet
             consectetur adipisicing elit. Ut, facere.
           </p>
-          <ul className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl">
+          <motion.ul
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.8, delay: 1 }}
+            className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl"
+          >
             {infoList.map(({ icon, iconDark, title, description }, index) => (
-              <li
+              <motion.li
+                whileInView={{ scale: 1.05 }}
                 className="border-[.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black"
                 key={index}
               >
                 <Image className="w-7 mt-3" src={icon} alt={title} />
                 <h3 className="my-4 font-semibold text-gray-700">{title}</h3>
                 <p className="text-gray-600 text-sm">{description}</p>
-              </li>
+              </motion.li>
             ))}
-          </ul>
+          </motion.ul>
           <h4 className="my-6 text-gray-700 font-main">Tools I use</h4>
           <ul className="flex items-center gap-3 sm:gap-5">
             {toolsData.map((tool, index) => (
@@ -53,8 +89,8 @@ const About = () => {
               </li>
             ))}
           </ul>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </motion.div>
   );
 };
