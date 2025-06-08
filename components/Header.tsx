361068d635b9f758480a2e3b7c8dec6a00bc4a5d
MogesBekele <mogesbekele32@gmail.com>
2025-06-08 18:03:23 +0300

    add the animation on the header h3

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,13 +6,22 @@ import {motion} from 'motion/react';
 const Header = () => {
   return (
     <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4 pt-32">
-      <motion.div>
+      <motion.div
+      initial={{scale:0}}
+      whileInView={{scale: 1}}
+      transition={{duration: 0.8, type: 'spring', stiffness: 100}}
+      >
         <Image alt="" src={assets.profile_img} className="rounded-full w-32" />
       </motion.div>
-      <h3 className="flex items-center gap-2 text-xl md:text-2xl mb-3 ">
+      <motion.h3
+      initial={{y:-20, opacity:0}}
+      whileInView={{y:0, opacity:1}}
+      transition={{duration: 0.6, delay:0.3}}
+      
+      className="flex items-center gap-2 text-xl md:text-2xl mb-3 ">
         Hi I'm Moges Bekele
         <Image src={assets.hand_icon} alt="" className="w-6" />
-      </h3>
+      </motion.h3>
       <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-main">
         Fullstack developer based on Ethoipia
       </h1>
