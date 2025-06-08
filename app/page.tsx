172b2a4c7c49863203da56897c350f6dcdd5d95e
MogesBekele <mogesbekele32@gmail.com>
2025-06-08 05:59:56 +0300

    on main page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Servives from "@/components/Servives";
 import Work from "@/components/Work";
-
 export default function Home() {
   return (
     <>
