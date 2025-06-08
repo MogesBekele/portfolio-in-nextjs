c27abc7c1201095d74488eea7a82e2eb5f78f09d
MogesBekele <mogesbekele32@gmail.com>
2025-06-08 18:41:48 +0300

    add the animation on contact section

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -31,50 +31,59 @@ const Contact = () => {
 
   return (
     <motion.div
-        initial={{ opacity: 0 }}
+      initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1 }}
-
       id="contact"
       className="w-full px-4 md:px-[8%] py-10 scroll-mt-20 pt-32 bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-[length:90%_auto]"
     >
       <motion.h4
-         initial={{ y: -20, opacity: 0 }}
+        initial={{ y: -20, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.3, delay: 0.5 }}
-      
-      className="text-center mb-2 text-lg font-ovo">Connect with me</motion.h4>
+        className="text-center mb-2 text-lg font-ovo"
+      >
+        Connect with me
+      </motion.h4>
       <motion.h2
-         initial={{ y: -20, opacity: 0 }}
+        initial={{ y: -20, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.5 }}
-      
-      className="text-center text-5xl font-ovo">Get in touch</motion.h2>
+        className="text-center text-5xl font-ovo"
+      >
+        Get in touch
+      </motion.h2>
       <motion.p
-         initial={{  opacity: 0 }}
+        initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 0.7, delay: 0.5 }}
-      
-      className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
+        className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo"
+      >
         I'd love to hear from you! If you have any questions, comments, or
         feedback please use the form below
       </motion.p>
       <motion.form
-         initial={{ y: -20, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.5 }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.9, delay: 0.5 }}
         onSubmit={onSubmit}
         className="max-w-2xl mx-auto flex flex-col gap-6 bg-white/80 p-8 rounded-lg shadow"
       >
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <input
+          <motion.input
+            initial={{ x: -50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1.1, delay: 0.6 }}
             type="text"
             name="name"
             placeholder="Enter your name"
             required
             className="border border-gray-300 rounded-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
           />
-          <input
+          <motion.input
+            initial={{ x: 50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1.2, delay: 0.6 }}
             type="email"
             name="email"
             placeholder="Enter your email"
@@ -82,13 +91,17 @@ const Contact = () => {
             className="border border-gray-300 rounded-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
           />
         </div>
-        <textarea
+        <motion.textarea
+
+                initial={{ y: 100, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1.3, delay: 0.6 }}
           rows={6}
           placeholder="Enter your message"
           name="message"
           required
           className="border border-gray-300 rounded-md px-4 py-3 focus:outline-none focus:ring-2 focus:ring-blue-400 transition resize-none"
-        ></textarea>
+        ></motion.textarea>
         <button
           type="submit"
           className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80
